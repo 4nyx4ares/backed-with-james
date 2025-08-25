1d024e6ba4db1532cfdc527c9cf8e904188c85d5
4nyx4ares <arash8shoja@gmail.com>
2025-08-25 17:01:17 +0330

    making some modifications

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const app = express()
-const PORT = 8383
+const app = express();
+const PORT = 8383;
 
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
 
-app.listen(PORT, (req, res)=>{
+app.get("/dashboard", (req, res) => {
+  res.send("hi");
+});
+app.listen(PORT, (req, res) => {
   console.log(`server has started at ${PORT}`);
-})
+});
